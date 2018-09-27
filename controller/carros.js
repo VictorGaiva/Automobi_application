7ef35e660837960c736f1ce24ed8e42c07cdf0a2
Victor <13839490+VictorGaiva@users.noreply.github.com>
2018-09-26 22:19:35 -0400

    Warns user of internal sever error and some refactoring

--- controller/carros.js
+++ controller/carros.js
@@ -5,13 +5,6 @@ const Joi           = require('joi')
 
 let router = express.Router()
 
-
-// C = Done
-// E = 
-// V = Done
-// L = Done
-// D =
-
 let schema = {
     "marcasativas":{
         'N': Joi.string().min(1).required(),
@@ -29,10 +22,19 @@ let schema = {
     }
 }
 
-let dataBase = (collection, func) => {
+const softClose = (err, res, connection) =>{
+    //Warn the user about an issue
+    res.status(500).send("Oh no. Something went wrong. Try again later.")
+    console.log(`Error: ${err}`)
+    //connection.close()
+    //throw err
+}
+
+const dataBase = (res, func) => {
     MongoClient.connect('mongodb://localhost:27017/test',{useNewUrlParser: true} , function (err, connection) {
-        if (err) throw err
-        func(connection.db('test'), connection)
+        if (err) softClose(err, res, connection)
+        else
+            func(connection.db('test'), connection)
     })
 }
 
@@ -56,41 +58,54 @@ router.route('/').all(function(req, res) {
 router.route('/:collection')
     //List
     .get(function(req, res) {
-        dataBase(req.params.collection, function (db, connection) {
-            db.collection(req.params.collection).find(req.query).toArray(function(err, result) {
-                if (err) throw err
-                res.send(result)
-                console.log(req.query)
-                connection.close()
+        //Open connection
+        dataBase(res, function (db, connection) {
+            //Select collection
+            db.collection(req.params.collection)
+                //Query data
+                .find(req.query).toArray(function(err, result) {
+                    if (err) softClose(err, res, connection)
+                    else{
+                        res.send(result)
+                        console.log(req.query)
+                        connection.close()
+                    }
             })
         })
     })
 
     //Create
     .post(function(req, res) {
-        dataBase(req.params.collection, function (db, connection) {
-            //Check if data is ok
-
+        dataBase(res, function (db, connection) {
+            //Validade data
             const valid = Joi.validate(req.body, schema[req.params.collection])
- 
+            
+            //If data is valid
             if(!valid.error){
-                db.collection(req.params.collection).insertOne(req.body, function(err, result) {
-                    if (err) throw err
-                    res.send("Data added to Database")
-                    console.log(`Added data to DB. ID=${result.insertedId}`)
-                    connection.close()
+                //Open the collection
+                db.collection(req.params.collection)
+                    //Insert data
+                    .insertOne(req.body, function(err, result) {
+                        if (err) softClose(err, res, connection)
+                        else{
+                            res.send("Data added to Database")
+                            console.log(`Added data to DB. ID=${result.insertedId}`)
+                            connection.close()
+                        }
                 })
             }
+            //Warn the user
             else{
                 const message = valid.error.details[0].message
                 res.status(400).send(`${message}`)
                 console.log(`Unable to add ${req.body}. Error: ${message}`)
-                return false
+                connection.close()
             }
         })
     })
+    //Update
     .put(function(req, res) {
-        dataBase(req.params.collection, function (db, connection) {
+        dataBase(res, function (db, connection) {
             //Check if data is valid
             const valid = Joi.validate(req.body, schema[req.params.collection])
             
@@ -113,11 +128,11 @@ router.route('/:collection')
 
                 //Check if data already exists on database
                 db.collection(req.params.collection).findOne({_id:id}, function(err, result) {
-                    if (err) throw err
+                    if (err) softClose(err, res, connection)
                     else if(result){
                         //If it does, update it
                         db.collection(req.params.collection).updateOne({_id:id}, {$set:req.body}, function(err, result) {
-                            if (err) throw err
+                            if (err) softClose(err, res, connection)
                             res.send(result)
                             console.log(`Updated data to DB. ID=${result}`)
                             connection.close()
@@ -127,11 +142,9 @@ router.route('/:collection')
                         //If it doesn't, inform the user
                         res.status(404).send("Item not found.")
                         console.log(err, result)
+                        connection.close()
                     }
-                    connection.close()
                 })
-            
-                
             }
             else{
                 //Get the error message
@@ -143,11 +156,11 @@ router.route('/:collection')
             }
         })
     })
+    //Delete
     .delete(function(req, res) {
-        dataBase(req.params.collection, function (db, connection) {
+        dataBase(res, function (db, connection) {
             //Check if data is valid
             const valid = Joi.validate(req.body, schema[req.params.collection])
-            
 
             if(!valid.error){
                 //Check if the ID is set
@@ -167,25 +180,26 @@ router.route('/:collection')
 
                 //Check if data exists on database
                 db.collection(req.params.collection).findOne({_id:id}, function(err, result) {
-                    if (err) throw err
+                    if (err) softClose(err, res, connection)
                     else if(result){
                         //If it does, delete it
                         db.collection(req.params.collection).deleteOne({_id:id}, function(err, result) {
-                            if (err) throw err
-                            res.send(result)
-                            console.log(`Item removed. ID=${result}`)
-                            connection.close()
+                            if (err) softClose(err, res, connection)
+                            else{
+                                res.send(result)
+                                console.log(`Item removed. ID=${result}`)
+                                connection.close()
+                            }
+                            
                         });
                     }
                     else{
                         //If it doesn't, inform the user
                         res.status(404).send("Item not found.")
                         console.log(err, result)
+                        connection.close()
                     }
-                    connection.close()
                 })
-            
-                
             }
             else{
                 //Get the error message
@@ -201,7 +215,7 @@ router.route('/:collection')
 
 //View
 router.route('/:collection/:id').get(function(req, res) {
-    dataBase(req.params.collection, function (db, connection) {
+    dataBase(res, function (db, connection) {
         //Validate the ID field
         let id = validateID(req.params.id)
 
@@ -214,7 +228,7 @@ router.route('/:collection/:id').get(function(req, res) {
         
         //Check if data already exists on database
         db.collection(req.params.collection).findOne({_id:id}, function(err, result) {
-            if (err) throw err
+            if (err) softClose(err, res, connection)
             else if(result){
                 //If it does, send the object to the user
                 res.send(result)
@@ -223,9 +237,9 @@ router.route('/:collection/:id').get(function(req, res) {
             else{
                 //If it doesn't, inform the user
                 res.status(404).send("Item not found.")
-                console.log(err, result)
+                console.log(`Querry of id "${id} is empty.`)
+                connection.close()
             }
-            connection.close()
         })
     })
 })
